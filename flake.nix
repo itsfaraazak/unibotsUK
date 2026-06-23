858605366d307bb79ce38ae3575b5b3a8b1cdce3
Faraaz Ali Khan <itsfaraazak@gmail.com>
2026-06-23 12:02:26 +0100

    Move to nix flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
+  };
+  outputs = { self, nix-ros-overlay, nixpkgs }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "UnibotsUK Fresher Force Software";
+          packages = with pkgs; [
+	    # Build tools
+            colcon
+	    cmake
+	    ninja
+	    pkg-config
+	    # Libraries
+	    ncnn
+	    (opencv.override { enableGtk3 = true; })
+	    #opencv
+	    # ... other non-ROS packages
+
+            (with pkgs.rosPackages.lyrical; buildEnv {
+              underlay = true;
+              paths = [
+                ros-core
+		rclcpp
+		sensor-msgs
+		cv-bridge
+		ament-cmake
+		ament-cmake-core
+		ament-index-cpp
+
+                # Gazebo + ROS Integration packages
+		#ros-gz 
+                #ros-gz-bridge
+                #ros-gz-sim
+                #ros-gz-image
+                # ... other ROS packages
+              ];
+            })
+          ];
+        };
+      });
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
